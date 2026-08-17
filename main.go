335554c43a52caba89e278f9f6b1911a8c8ed44b
Terje Sannum <terje.sannum@nav.no>
2026-08-17 18:20:46 +0200

    add metrics

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync/atomic"
+	"time"
 
 	"github.com/evcc-io/evcc/util"
 	"github.com/evcc-io/evcc/util/request"
 	"github.com/evcc-io/evcc/vehicle/polestar"
 	"github.com/hasura/go-graphql-client"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors/version"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"golang.org/x/oauth2"
 )
 
@@ -49,7 +54,6 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "query failed: %v\n", err)
 	}
-	fmt.Printf("cars: %v\n\n", carsRes)
 
 	publicClient := graphql.NewClient(publicApiURI, &http.Client{Transport: &apiKeyTransport{key: publicApiKey}})
 
@@ -66,19 +70,56 @@ func main() {
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "query failed: %v\n", err)
 		}
-		fmt.Printf("images: %v\n\n", imagesRes)
+
+		carInfo.WithLabelValues(car.VIN, car.ModelYear, car.ModelName, car.RegistrationNo).Set(1)
+		for _, img := range imagesRes.GetCarImages.Transparent {
+			carImage.WithLabelValues(car.VIN, img.URL, fmt.Sprintf("%d", img.Angle), "transparent").Set(1)
+		}
+		for _, img := range imagesRes.GetCarImages.Opaque {
+			carImage.WithLabelValues(car.VIN, img.URL, fmt.Sprintf("%d", img.Angle), "opaque").Set(1)
+		}
+		fmt.Printf("Found car %s (%s)\n", car.VIN, car.RegistrationNo)
 	}
 
 	for _, car := range carsRes.GetConsumerCarsV2 {
-		var telemetryRes struct {
-			CarTelemetryData `graphql:"carTelematicsV2(vins: $vins)"`
-		}
-
-		err = client.Query(ctx, &telemetryRes, map[string]any{
-			"vins": []string{car.VIN},
-		}, graphql.OperationName("CarTelematicsV2"))
-
-		fmt.Printf("telemetry: %v\n\n", telemetryRes)
+		ticker := time.NewTicker(time.Minute)
+		quit := make(chan struct{})
+		var atomicTelemetry atomic.Pointer[CarTelemetryData]
+		atomicTelemetry.Store(&CarTelemetryData{})
+		prometheus.MustRegister(NewCollector(car.VIN, &atomicTelemetry))
+		go func() {
+			fetchTelemetry := func() {
+				fmt.Println("Querying telemetry for", car.VIN)
+				var tempRes struct {
+					CarTelemetryData `graphql:"carTelematicsV2(vins: $vins)"`
+				}
+				err = client.Query(ctx, &tempRes, map[string]any{
+					"vins": []string{car.VIN},
+				}, graphql.OperationName("CarTelematicsV2"))
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "query failed: %v\n", err)
+					return
+				}
+				atomicTelemetry.Store(&tempRes.CarTelemetryData)
+			}
+			fetchTelemetry()
+			for {
+				select {
+				case <-ticker.C:
+					fetchTelemetry()
+				case <-quit:
+					ticker.Stop()
+					return
+				}
+			}
+		}()
 	}
 
+	prometheus.MustRegister(version.NewCollector("polestar_exporter"))
+	http.Handle("/metrics", promhttp.Handler())
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "http server failed: %v\n", err)
+	}
+	time.Sleep(time.Second * 300)
 }
---
+++ metrics.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"sync/atomic"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	carInfo = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "polestar_info",
+		Help: "Information about the car",
+	}, []string{"vin", "model_year", "model_name", "registration_no"})
+	carImage = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "polestar_image",
+		Help: "Car image",
+	}, []string{"vin", "url", "angle", "type"})
+)
+
+type TelemetryCollector struct {
+	telemetry              *atomic.Pointer[CarTelemetryData]
+	serviceSeconds         *prometheus.Desc
+	serviceMeters          *prometheus.Desc
+	odometerMeters         *prometheus.Desc
+	chargingStatus         *prometheus.Desc
+	chargingTimeToFull     *prometheus.Desc
+	batteryLevel           *prometheus.Desc
+	batteryDistanceToEmpty *prometheus.Desc
+}
+
+func NewCollector(vin string, telemetry *atomic.Pointer[CarTelemetryData]) *TelemetryCollector {
+	labels := prometheus.Labels{"vin": vin}
+	return &TelemetryCollector{
+		telemetry: telemetry,
+		serviceSeconds: prometheus.NewDesc(
+			"polestar_service_remaining_seconds",
+			"Number of seconds until the next service",
+			nil,
+			labels),
+		serviceMeters: prometheus.NewDesc(
+			"polestar_service_remaining_meters",
+			"Distance in meters until the next required service",
+			nil,
+			labels),
+		odometerMeters: prometheus.NewDesc(
+			"polestar_odometer_meters_total",
+			"Odometer reading in meters",
+			nil,
+			labels),
+		chargingStatus: prometheus.NewDesc(
+			"polestar_charging",
+			"Charging status of the car (1 = charging, 0 = not charging)",
+			nil,
+			labels),
+		chargingTimeToFull: prometheus.NewDesc(
+			"polestar_charging_time_to_full_seconds",
+			"Estimated time to full charge in seconds",
+			nil,
+			labels),
+		batteryLevel: prometheus.NewDesc(
+			"polestar_battery_level_percent",
+			"Battery charge level in percent",
+			nil,
+			labels),
+		batteryDistanceToEmpty: prometheus.NewDesc(
+			"polestar_battery_distance_to_empty_meters",
+			"Estimated distance to empty in meters",
+			nil,
+			labels),
+	}
+}
+
+func (c *TelemetryCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.serviceSeconds
+	ch <- c.serviceMeters
+	ch <- c.odometerMeters
+	ch <- c.chargingStatus
+	ch <- c.chargingTimeToFull
+	ch <- c.batteryLevel
+	ch <- c.batteryDistanceToEmpty
+}
+
+func metricTimestamp(ts EventUpdatedTimestamp) (time.Time, error) {
+	seconds, err := strconv.ParseInt(ts.Seconds, 10, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "invalid timestamp: %v\n", ts)
+		return time.Time{}, err
+	}
+	return time.Unix(seconds, ts.Nanos), nil
+}
+
+func (c *TelemetryCollector) Collect(ch chan<- prometheus.Metric) {
+	data := c.telemetry.Load()
+	if data == nil {
+		return
+	}
+
+	if len(data.Health) == 1 {
+		var ts, err = metricTimestamp(data.Health[0].Timestamp)
+		if err == nil {
+			ch <- prometheus.NewMetricWithTimestamp(
+				ts,
+				prometheus.MustNewConstMetric(
+					c.serviceSeconds,
+					prometheus.GaugeValue,
+					float64(data.Health[0].DaysToService*24*60*60)))
+			ch <- prometheus.NewMetricWithTimestamp(
+				ts,
+				prometheus.MustNewConstMetric(
+					c.serviceMeters,
+					prometheus.GaugeValue,
+					float64(data.Health[0].DistanceToServiceKm*1000)))
+		}
+	}
+
+	if len(data.Battery) == 1 {
+		var ts, err = metricTimestamp(data.Battery[0].Timestamp)
+		if err == nil {
+			var charging float64
+			if data.Battery[0].ChargingStatusV2 == "CHARGING_STATUS_V2_CHARGING" {
+				charging = 1
+			} else {
+				charging = 0
+			}
+			ch <- prometheus.NewMetricWithTimestamp(
+				ts,
+				prometheus.MustNewConstMetric(
+					c.chargingStatus,
+					prometheus.GaugeValue,
+					charging))
+			ch <- prometheus.NewMetricWithTimestamp(
+				ts,
+				prometheus.MustNewConstMetric(
+					c.chargingTimeToFull,
+					prometheus.GaugeValue,
+					float64(data.Battery[0].EstimatedChargingTimeToFullMinutes*60)))
+			ch <- prometheus.NewMetricWithTimestamp(
+				ts,
+				prometheus.MustNewConstMetric(
+					c.batteryLevel,
+					prometheus.GaugeValue,
+					float64(data.Battery[0].BatteryChargeLevelPercentage)))
+			ch <- prometheus.NewMetricWithTimestamp(
+				ts,
+				prometheus.MustNewConstMetric(
+					c.batteryDistanceToEmpty,
+					prometheus.GaugeValue,
+					float64(data.Battery[0].EstimatedDistanceToEmptyKm*1000)))
+		}
+	}
+
+	if len(data.Odometer) == 1 {
+		var ts, err = metricTimestamp(data.Odometer[0].Timestamp)
+		if err == nil {
+			ch <- prometheus.NewMetricWithTimestamp(
+				ts,
+				prometheus.MustNewConstMetric(
+					c.odometerMeters,
+					prometheus.CounterValue,
+					float64(data.Odometer[0].OdometerMeters)))
+		}
+	}
+
+}
